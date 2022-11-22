c30c321b2c2df996528b2b18851c46a18fcc1db6
kswarcher <kswarcher@gmail.com>
2022-11-22 10:07:14 -0600

    Standardizing and commenting

--- code/random_domain.ps1
+++ code/random_domain.ps1
@@ -1,8 +1,9 @@
+#Getting the output JSON file, number of users, groups, and local admins
 param( 
     [Parameter(Mandatory=$true)] $OutputJSONFile,
-    [int]$num_users,
-    [int]$num_groups,
-    [int]$num_localAdmin
+    [int]$UserAmount,
+    [int]$GroupAmount,
+    [int]$LocalAdminAmount
     )
 
 #Getting data from files
@@ -12,29 +13,29 @@ $first_names = [System.Collections.ArrayList](Get-Content "data/first_names.txt"
 $last_names = [System.Collections.ArrayList](Get-Content "data/last_names.txt")
 $passwords = [System.Collections.ArrayList](Get-Content "data/passwords.txt")
 
-##Making 10 random groups
+#Setting an amount of users and groups if not given
 $groups = @()
 $users = @()
 
-if ($num_users -eq 0){
-    $num_users = 5
+if ($UserAmount -eq 0){
+    $UserAmount = 5
 }
-if ($num_groups -eq 0){
-    $num_groups = 1
+if ($GroupAmount -eq 0){
+    $GroupAmount = 1
 }
 
-
-if ($num_localAdmin -ne 0){
+#Making some of the users become local admins
+if ($LocalAdminAmount -ne 0){
     $local_admin_indexes = @()
     while(($local_admin_indexes | Measure-Object).Count -lt $num_localAdmin){
 
-        $random_index = (Get-Random -InputObject (1..$num_users) | Where-Object{$local_admin_indexes -notcontains $_})
+        $random_index = (Get-Random -InputObject (1..$UserAmount) | Where-Object{$local_admin_indexes -notcontains $_})
         $local_admin_indexes += $random_index
     }
 }
 
-
-for ( $i = 1; $i -le $num_groups; $i++){
+#Making random groups
+for ($i = 1; $i -le $GroupAmount; $i++){
 $group_name = (Get-Random -InputObject $group_names )
 $group = @{"name"="$group_name"}
 $groups+= $group
@@ -42,9 +43,8 @@ $group_names.Remove($group_name)
 }
 
 
-##Making random users
-
-for ( $i = 1; $i -le $num_users; $i++){
+#Making random users
+for ( $i = 1; $i -le $UserAmount; $i++){
 $first_name = (Get-Random -InputObject $first_names)
 $last_name = (Get-Random -InputObject $last_names)
 $password = (Get-Random -InputObject $passwords)
@@ -54,10 +54,11 @@ $new_user = @{
     "groups" = @( (Get-Random -InputObject $groups).name)
 }
 
+#Assigning users to be admins
 if($local_admin_indexes | Where {$_ -eq $i}){
     $new_user["local_admin"] = $true
 }
-
+#Adding users credentials
 $users+=$new_user
 $first_names.Remove($first_name)
 $last_names.Remove($last_name)
@@ -66,8 +67,7 @@ $passwords.Remove($password)
 }
 
 
-##Making the JSON
-
+#Making the JSON
 @{
 "domain" = "xyz.com"
 "groups"=$groups
